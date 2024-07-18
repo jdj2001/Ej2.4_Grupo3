6905be04b2fe9c72d9d19e8bdbbadfe354a30f91
Juan Alvarado <juandjesus.2001@gmail.com>
2024-07-17 18:32:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ej24_grupo3/SQLiteConexion.java
@@ -0,0 +1,26 @@
+package com.example.ej24_grupo3;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.ej24_grupo3.transacciones.Transacciones;
+
+public class SQLiteConexion extends SQLiteOpenHelper {
+
+    public SQLiteConexion(Context context, String bddname, SQLiteDatabase.CursorFactory factory, int version){
+        super(context, bddname, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Transacciones.CreateTableFirmas);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(Transacciones.DropTableFirmas);
+        onCreate(db);
+    }
+
+}
